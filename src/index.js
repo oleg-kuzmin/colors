42c9efda3b8e1db2a154ca7989ffb9acc7e27817
Oleg Kuzmin <info@oleg-kuzmin.ru>
2024-11-10 18:05:17 +0300

    feat: add js-logic for filters

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 import './index.scss';
 import './scripts/swiper';
 import './scripts/modal';
+import './scripts/filters-mobile';
---
+++ src/scripts/filters-mobile.js
@@ -0,0 +1,32 @@
+const buttonFilters = document.querySelector('.button-filters');
+const filters = document.querySelector('.filters');
+
+function openFilter() {
+  filters.classList.add('filters_opened');
+}
+
+function closeFilters() {
+  filters.classList.remove('filters_opened');
+}
+
+buttonFilters.addEventListener('click', () => {
+  openFilter();
+  let dotOne = 0;
+  let dotTwo = 0;
+
+  const handleTouchStart = evt => {
+    dotOne = evt.changedTouches[0].clientY;
+  };
+
+  const handleTouchEnd = evt => {
+    dotTwo = evt.changedTouches[0].clientY;
+    if (dotTwo > dotOne + 5) {
+      filters.removeEventListener('touchstart', handleTouchStart);
+      filters.removeEventListener('touchend', handleTouchEnd);
+      closeFilters();
+    }
+  };
+
+  filters.addEventListener('touchstart', handleTouchStart);
+  filters.addEventListener('touchend', handleTouchEnd);
+});
