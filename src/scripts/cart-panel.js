9781fd26a986312b3d69a254e88f6e64d3db74a6
Oleg Kuzmin <info@oleg-kuzmin.ru>
2024-11-11 10:54:35 +0300

    feat: add cart-footer

--- src/scripts/cart-panel.js
+++ src/scripts/cart-panel.js
@@ -1,3 +1,11 @@
-const linkBasket = document.querySelector('.button-basket');
+const buttonBasket = document.querySelector('.button-basket');
+const cartPanel = document.querySelector('.cart');
+const cartButtonClose = document.querySelector('.cart__button-close');
 
-console.log(linkBasket);
+buttonBasket.addEventListener('click', () => {
+  cartPanel.classList.add('cart_opened');
+});
+
+cartButtonClose.addEventListener('click', () => {
+  cartPanel.classList.remove('cart_opened');
+});
